8d98d74b272231f3e95b489031ab75f31997e666
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-10-06 22:16:00 +0300

    feat: добавить метод генерации со стартовой координатой

--- src/main/java/backend/academy/mazes/generator/Generator.java
+++ src/main/java/backend/academy/mazes/generator/Generator.java
@@ -1,9 +1,12 @@
 package backend.academy.mazes.generator;
 
+import backend.academy.mazes.maze.Coordinate;
 import backend.academy.mazes.maze.Maze;
 
 public interface Generator {
 
     Maze generate(int height, int width);
+
+    Maze generate(int height, int width, Coordinate start);
 
 }
