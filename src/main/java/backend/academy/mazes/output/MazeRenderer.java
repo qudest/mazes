75ea951b34a4abd533463ed4281a4e60d74676f5
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-10-08 15:53:51 +0300

    feat: добавить рендер начала и конца пути

--- src/main/java/backend/academy/mazes/output/MazeRenderer.java
+++ src/main/java/backend/academy/mazes/output/MazeRenderer.java
@@ -11,9 +11,12 @@ public class MazeRenderer implements Renderer {
     private static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
     private static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
     private static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    private static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     private static final String PASSAGE_SPRITE = ANSI_WHITE_BACKGROUND + "   " + ANSI_RESET;
     private static final String WALL_SPRITE = ANSI_BLACK_BACKGROUND + "   " + ANSI_RESET;
     private static final String PATH_SPRITE = ANSI_GREEN_BACKGROUND + "   " + ANSI_RESET;
+    private static final String START_SPRITE = ANSI_CYAN_BACKGROUND + "   " + ANSI_RESET;
+    private static final String END_SPRITE = START_SPRITE;
     private static final String LINE_BREAK = "\n";
 
     @Override
@@ -22,12 +25,7 @@ public class MazeRenderer implements Renderer {
         Cell[][] grid = maze.getGrid();
         for (Cell[] row : grid) {
             for (Cell cell : row) {
-                if (cell.type().equals(Cell.Type.WALL)) {
-                    stringBuilder.append(WALL_SPRITE);
-                }
-                if (cell.type().equals(Cell.Type.PASSAGE)) {
-                    stringBuilder.append(PASSAGE_SPRITE);
-                }
+                stringBuilder.append(renderCell(cell, null, null));
             }
             stringBuilder.append(LINE_BREAK);
         }
@@ -42,19 +40,32 @@ public class MazeRenderer implements Renderer {
             for (int col = 0; col < grid[row].length; col++) {
                 Cell cell = grid[row][col];
                 Coordinate coordinate = new Coordinate(row, col);
-                if (cell.type().equals(Cell.Type.WALL)) {
-                    stringBuilder.append(WALL_SPRITE);
-                } else if (cell.type().equals(Cell.Type.PASSAGE)) {
-                    if (path.contains(coordinate)) {
-                        stringBuilder.append(PATH_SPRITE);
-                    } else {
-                        stringBuilder.append(PASSAGE_SPRITE);
-                    }
-                }
+                stringBuilder.append(renderCell(cell, coordinate, path));
             }
             stringBuilder.append(LINE_BREAK);
         }
         return stringBuilder.toString();
+    }
+
+    private String renderCell(Cell cell, Coordinate cellCoordinate, List<Coordinate> path) {
+        if (cell.type().equals(Cell.Type.WALL)) {
+            return WALL_SPRITE;
+        } else {
+            return renderPassage(cellCoordinate, path);
+        }
+    }
+
+    private String renderPassage(Coordinate cellCoordinate, List<Coordinate> path) {
+        if (path != null && !path.isEmpty()) {
+            if (path.getFirst().equals(cellCoordinate)) {
+                return START_SPRITE;
+            } else if (path.getLast().equals(cellCoordinate)) {
+                return END_SPRITE;
+            } else if (path.contains(cellCoordinate)) {
+                return PATH_SPRITE;
+            }
+        }
+        return PASSAGE_SPRITE;
     }
 
 }
