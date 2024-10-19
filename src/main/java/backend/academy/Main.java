2c8c22c3f458d86f488643b786fd78075420af03
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-10-19 22:35:54 +0300

    feat: запуск программы

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,11 +1,31 @@
 package backend.academy;
 
+import backend.academy.mazes.MazeManager;
+import backend.academy.mazes.generator.HuntAndKill;
+import backend.academy.mazes.generator.RecursiveBacktrackerGenerator;
+import backend.academy.mazes.input.ConsoleInput;
+import backend.academy.mazes.output.ConsoleOutput;
+import backend.academy.mazes.output.MazeRenderer;
+import backend.academy.mazes.solver.DijkstraSolver;
+import backend.academy.mazes.solver.RecursiveBacktrackerSolver;
 import lombok.experimental.UtilityClass;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
 
 @UtilityClass
 public class Main {
 
     public static void main(String[] args) {
+        MazeManager mazeManager =
+            new MazeManager(
+                new ConsoleInput(new BufferedReader(new InputStreamReader(System.in))),
+                new MazeRenderer(),
+                new ConsoleOutput(System.out),
+                List.of(new RecursiveBacktrackerGenerator(), new HuntAndKill()),
+                List.of(new RecursiveBacktrackerSolver(), new DijkstraSolver())
+            );
+        mazeManager.start();
     }
 
 }
